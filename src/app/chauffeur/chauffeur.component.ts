5f5edababb328ef30c038cc598675ae710461dcd
rmy17 mail = remyvi17@gmail.com <remyvi17@gmail.com>
2019-05-20 15:21:52 +0200

    update angular6 to angular7 and start driver

---
+++ src/app/chauffeur/chauffeur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chauffeur',
+  template: './chauffer.components.html',
+  styles: []
+})
+export class ChauffeurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
