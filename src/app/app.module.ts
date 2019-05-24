75a9c8a32edb8b188b27bef11b500f745d0ff464
rmy17 mail = remyvi17@gmail.com <remyvi17@gmail.com>
2019-05-24 17:33:02 +0200

    #13 developpement du planning en cours

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { TechComponent } from './tech/tech.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AuthComponent } from './auth/auth.component';
-import {FormsModule} from "@angular/forms";
-import {StatutConnecteService} from "./auth/statut-connecte.service";
-import {AuthInterceptorService} from "./auth/auth-interceptor.service";
+import { FormsModule } from "@angular/forms";
+import { StatutConnecteService } from "./auth/statut-connecte.service";
+import { AuthInterceptorService } from "./auth/auth-interceptor.service";
 import { PlanningComponent } from './planning/planning.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [
   { path: 'tech', component: TechComponent, canActivate:[StatutConnecteService]}, // /tech accessible uniquement si connecté
   { path: 'auth', component: AuthComponent},
-  { path: 'chauffeur/planning', component: AuthComponent}
+  { path: 'chauffeur/planning', component: PlanningComponent},
   { path: '', redirectTo: '/tech', pathMatch: 'full'}
 ];
 
@@ -32,7 +35,9 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     HttpClientModule,
     MDBBootstrapModule.forRoot(),
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
