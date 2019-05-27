fca47ebc30a7be24ca43abd9dc7c4decd5e6a24d
rmy17 mail = remyvi17@gmail.com <remyvi17@gmail.com>
2019-05-27 17:07:54 +0200

    fin de gestion chauffeur

--- src/app/chauffeur/chauffeur.component.ts
+++ src/app/chauffeur/chauffeur.component.ts
@@ -27,55 +27,6 @@ export class ChauffeurComponent implements OnInit {
     this._serv.recupCollegueChauffeur().subscribe(chauffeur => this.tabChauffeur = chauffeur, err => this.message = `${err}`);
   }
 
-  /*
-  tri() {
-    this.tabTrierChauffeur = [];
-    for ( let chauf of this.tabChauffeur) {
-      console.log(this.mat)
-      console.log(this.tabTrierChauffeur);
-      if (this.mat != undefined && this.nom != undefined && this.prenom != undefined) {
-        if (chauf.matricule === this.mat && chauf.nom === this.nom && chauf.prenom === this.prenom) {
-          this.tabTrierChauffeur.push(chauf);
-        }
-      }
-      else if (this.mat != undefined && this.nom != undefined) {
-        if (chauf.matricule === this.mat && chauf.nom === this.nom) {
-          this.tabTrierChauffeur.push(chauf);
-        }
-      }
-      else if (this.mat != undefined && this.prenom != undefined) {
-        if (chauf.matricule === this.mat && chauf.prenom === this.prenom) {
-          this.tabTrierChauffeur.push(chauf);
-        }
-      }
-      else if (this.nom != undefined && this.prenom != undefined) {
-        if (chauf.nom === this.nom && chauf.prenom === this.prenom) {
-          this.tabTrierChauffeur.push(chauf);
-        }
-      }
-      else if (this.mat != undefined) {
-        if (chauf.matricule === this.mat) {
-          this.tabTrierChauffeur.push(chauf);
-
-        }
-      }
-      else if (this.nom != undefined) {
-        console.log(chauf);
-        if (chauf.nom === this.nom) {
-          this.tabTrierChauffeur.push(chauf);
-        }
-      }
-      else if (this.prenom != undefined) {
-        if (chauf.prenom === this.prenom) {
-          this.tabTrierChauffeur.push(chauf);
-        }
-      }
-      else if (this.mat === undefined && this.nom === undefined && this.prenom === undefined) {
-        this.tabTrierChauffeur = this.tabChauffeur;
-      }
-    }
-  }*/
-
   triMat() {
     this.tabTrierChauffeur = this.tabChauffeur.filter(chauf => this.mat == chauf.id);
     this.trier=true;
