a9505042f0d40f91116f305692790cd24ba8c18f
rmy17 mail = remyvi17@gmail.com <remyvi17@gmail.com>
2019-05-27 11:12:10 +0200

    ajout modifiaction app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,19 +15,16 @@ import { CreationReservationComponent } from './creation-reservation/creation-re
 import { ImmatriculationValidatorDirective } from './validator/immatriculation-validator.directive';
 import { ChauffeurComponent } from './chauffeur/chauffeur.component';
 import { FilterPipe } from './filter.pipe';
-<<<<<<< HEAD
 import { CycleVieVehiculeComponent } from './vehicule-gestion/cycle-vie-vehicule/cycle-vie-vehicule.component';
-=======
 import { LireReservationComponent } from './lire-reservation/lire-reservation.component';
 import { VehiculeGestionComponent } from './vehicule-gestion/vehicule-gestion.component';
 import { PhotoUrlValidatorDirective } from './validator/photo-url-validator.directive';
->>>>>>> master
 
 const routes: Routes = [
   { path:'tech', component: TechComponent, canActivate:[StatutConnecteService]}, // /tech accessible uniquement si connecté
   { path:'auth', component: AuthComponent},
-  {path:'reserver', component:ReservationComponent},
-  {path:'reservations', component:LireReservationComponent},
+  {path:'collaborateur/reserver', component:ReservationComponent},
+  {path:'collaborateur/reservations', component:LireReservationComponent},
   { path:'admin/vehicules', component: VehiculeGestionComponent},
   {path: 'admin/vehicules/:immatriculation', component: CycleVieVehiculeComponent},
   {path: 'admin/chauffeur', component: ChauffeurComponent},
