40c4a41370ae3e8075de9622a219730b3de2142a
Amadou <amadoudem@sfr.fr>
2019-05-27 09:09:38 +0200

    build error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,28 +10,18 @@ import { AuthComponent } from './auth/auth.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {StatutConnecteService} from "./auth/statut-connecte.service";
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
-<<<<<<< HEAD
-import { ReservationComponent } from './reservation/reservation.component';
-import { CreationReservationComponent } from './creation-reservation/creation-reservation.component';
-=======
 import { VehiculeGestionComponent } from './vehicule-gestion/vehicule-gestion.component';
 import { PhotoUrlValidatorDirective } from './validator/photo-url-validator.directive';
 import { ImmatriculationValidatorDirective } from './validator/immatriculation-validator.directive';
 import { ChauffeurComponent } from './chauffeur/chauffeur.component';
 import { FilterPipe } from './filter.pipe';
->>>>>>> f25565ea5a12884eda434916e855e3582334d381
 
 const routes: Routes = [
 
   { path:'tech', component: TechComponent, canActivate:[StatutConnecteService]}, // /tech accessible uniquement si connecté
   { path:'auth', component: AuthComponent},
-<<<<<<< HEAD
-  {path:'collaborateur/reservations/creer', component:ReservationComponent},
-=======
   { path:'admin/vehicules', component: VehiculeGestionComponent},
   {path: 'admin/chauffeur', component: ChauffeurComponent},
-
->>>>>>> f25565ea5a12884eda434916e855e3582334d381
   { path: '', redirectTo: '/tech', pathMatch: 'full'}
 ];
 
@@ -41,18 +31,12 @@ const routes: Routes = [
     AppComponent,
     TechComponent,
     AuthComponent,
-<<<<<<< HEAD
-    ReservationComponent,
-    ReservationComponent,
-    CreationReservationComponent
-=======
     VehiculeGestionComponent,
     PhotoUrlValidatorDirective,
     ImmatriculationValidatorDirective,
     ChauffeurComponent,
     FilterPipe
 
->>>>>>> f25565ea5a12884eda434916e855e3582334d381
   ],
   imports: [
     BrowserModule,
